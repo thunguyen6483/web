0dee0e812df3114333df0ad6dca4a2e3d819934a
thunguyen6483 <108853661+thunguyen6483@users.noreply.github.com>
2022-07-07 18:44:37 +0700

    Update webTV.py

--- webTV.py
+++ webTV.py
@@ -1,64 +1,113 @@
 import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
-st.set_page_config(page_title="web của tôi" , page_icon=":tada:",layout="wide")
+from PIL import Image
+
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+
 def load_lottieurl(url):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
-lottie_coding = load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_o6spyjnc.json")
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            .css-1rs6os {visibility: hidden;}
-            .css-17ziqus {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
-with st.container():
-    st.write("---")
-    left_column , right_column = st.columns(2)
-    with left_column:
-        st.subheader("chào , tôi là quân :wave:")
-        st.title("một học sinh đến từ Vietnam")
-        st.write("Tôi đam mê tìm cách sử dụng Python để ngày càng hiệu quả hơn")
-        st.write("[learn more>](https://www.youtube.com/channel/UC3opf8J6aPKbP4EZj892rwQ)")
-with st.container():
-    st.write("---")
-    left_column , right_column = st.columns(2)
-    with left_column:
-         st.header("việc tôi làm")
-         st.write("##")
-         st.write("""
-         Trên kênh YouTube của mình, tôi đang tạo các hướng dẫn cho những người:
-         - đang tìm cách tận dụng sức mạnh của Python trong công việc hàng ngày của họ.
-         - đang làm việc với Excel và tự nhận thấy rằng - "phải có một cách tốt hơn." """ 
-         )
-         st.write("[learn more>](https://www.youtube.com/channel/UC3opf8J6aPKbP4EZj892rwQ)")
-    with right_column:
-            st_lottie(lottie_coding, height = 300 )
-with st.container():
-    st.write("---")
-    st.header("liên lạc với tôi")
-    st.write('##')
-    contact_form = """
-    <form action="https://formsubmit.co/quanpham122007@gmail.com" method="POST">
-    <input type="hidden" name="_captcha" value="false">
-    <input type="text" name="name" placeholder ="tên của bạn" required>
-    <input type="email" name="email" placeholder="email của bạn" required>
-    <textarea name="message" placeholder="Chi tiết về vấn đề của bạn"></textarea>
-    <button type="submit">Send</button>
-</form>
-"""
+
+
+# Use local CSS
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
 local_css("style/style.css")
-left_column , right_column = st.columns(2)
-with left_column :
-    st.markdown(contact_form, unsafe_allow_html=True)
-with right_column:
-    st.empty()
+
+# ---- LOAD ASSETS ----
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
+img_contact_form = Image.open("images/yt_contact_form.png")
+img_lottie_animation = Image.open("images/yt_lottie_animation.png")
+
+# ---- HEADER SECTION ----
+with st.container():
+    st.subheader("Hi, I am Sven :wave:")
+    st.title("A Data Analyst From Germany")
+    st.write(
+        "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings."
+    )
+    st.write("[Learn More >](https://pythonandvba.com)")
+
+# ---- WHAT I DO ----
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(
+            """
+            On my YouTube channel I am creating tutorials for people who:
+            - are looking for a way to leverage the power of Python in their day-to-day work.
+            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
+            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
+            - are working with Excel and found themselves thinking - "there has to be a better way."
+            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
+            """
+        )
+        st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
+    with right_column:
+        st_lottie(lottie_coding, height=300, key="coding")
+
+# ---- PROJECTS ----
+with st.container():
+    st.write("---")
+    st.header("My Projects")
+    st.write("##")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(img_lottie_animation)
+    with text_column:
+        st.subheader("Integrate Lottie Animations Inside Your Streamlit App")
+        st.write(
+            """
+            Learn how to use Lottie Files in Streamlit!
+            Animations make our web app more engaging and fun, and Lottie Files are the easiest way to do it!
+            In this tutorial, I'll show you exactly how to do it
+            """
+        )
+        st.markdown("[Watch Video...](https://youtu.be/TXSOitGoINE)")
+with st.container():
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(img_contact_form)
+    with text_column:
+        st.subheader("How To Add A Contact Form To Your Streamlit App")
+        st.write(
+            """
+            Want to add a contact form to your Streamlit website?
+            In this video, I'm going to show you how to implement a contact form in your Streamlit app using the free service ‘Form Submit’.
+            """
+        )
+        st.markdown("[Watch Video...](https://youtu.be/FOULV9Xij_8)")
+
+# ---- CONTACT ----
+with st.container():
+    st.write("---")
+    st.header("Get In Touch With Me!")
+    st.write("##")
+
+    # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
+    contact_form = """
+    <form action="https://formsubmit.co/YOUR@MAIL.COM" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
